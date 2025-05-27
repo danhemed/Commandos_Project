981f3903d82e8395a99f9f0d50753512e39b589d
Dan <danhemed123@gmail.com>
2025-05-27 16:45:54 +0300

    add a three files of Factory

--- Program.cs
+++ Program.cs
@@ -11,7 +11,35 @@ namespace CommandosProject.Modeles
     {
         public static void Main(string[] args)
         {
-            
+            Commando soldier = new Commando("boomy", "x65", ["Hammer", "Chisel", "rope", "bag", "WaterBottle"], "stand");
+            soldier.Walk();
+            soldier.Hide();
+            soldier.Attack();
+            soldier.NameCode = "Y77";
+            soldier.SayName("colonel");
+            soldier.SayName("general");
+            soldier.SayName("");
+            Console.WriteLine();
+
+            Weapon weapon = new Weapon("gon", "apple", 10);
+            weapon.Shoot();
+            Console.WriteLine();
+
+            Commando[] arraySoldier =
+            {
+                new Commando("soldier1", "r17", ["Hammer", "Chisel", "rope", "bag", "WaterBottle"], "stand"),
+                new AirCommando("soldier2", "a90", ["Hammer", "Chisel", "rope", "bag", "WaterBottle"], "stand"),
+                new SeaCommando("soldier3", "j88", ["Hammer", "Chisel", "rope", "bag", "WaterBottle"], "stand")
+            };
+
+            foreach (var sold in arraySoldier)
+            {
+                sold.SayName("General");
+                sold.Walk();
+                sold.Hide();
+                sold.Attack();
+                Console.WriteLine();
+            }
         }
     }
 }
