34d93edcb48a53a5326cbe0cdd15e6cc2b1db503
Dan <danhemed123@gmail.com>
2025-05-28 09:50:03 +0300

    add tow weapon in weaponFactory

--- WeaponFactory.cs
+++ WeaponFactory.cs
@@ -8,7 +8,20 @@ namespace CommandosProject.Modeles
 {
     public class WeaponFactory
     {
-        Weapon[] weaponArr = 
+        public string Name;
+        public int Width;
+        public string Color;
+        public string Status;
+
+        public WeaponFactory(string name, int width, string color, string status)
+        {
+            Name = name;
+            Width = width;
+            Color = color;
+            Status = status;
+        }
+
+        Weapon[] weaponArr =
         {
             new Weapon("M16", "COLT", 29),
             new Weapon("AK47", "KALASHNIKOV", 30),
@@ -17,10 +30,25 @@ namespace CommandosProject.Modeles
 
     public class Stone : WeaponFactory
     {
-        public string Name;
-        public int Width;
-        public string Status;
         public int SumBoomBroken;
         public int SumBoom;
+
+        public Stone(string name, int width, string color, string status, int sumBoomBroken, int sumBoom) : base(name, width, color, status)
+        {
+            SumBoomBroken = sumBoomBroken;
+            SumBoom = sumBoom;
+        }
+    }
+
+    public class Knife : WeaponFactory
+    {
+        public string TypeMetal;
+        public string Producer;
+
+        public Knife(string name, int width, string color, string status, string typeMetal, string producer) : base(name, width, color, status)
+        {
+            TypeMetal = typeMetal;
+            Producer = producer;
+        }
     }
 }
