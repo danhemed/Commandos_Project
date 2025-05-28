603b96f4b6de8d3c361926763f0e34386f0df77d
Dan <danhemed123@gmail.com>
2025-05-28 17:38:48 +0300

    change in weapon class and add toe file WeaponCold and WeaponHot

---
+++ WeaponHot.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CommandosProject.Modeles
+{
+    public class Weapon
+    {
+        public string Name;
+        public string Producer;
+        public int Bullets;
+
+        public Weapon(string name, string producer, int bullets)
+        {
+            Name = name;
+            Producer = producer;
+            Bullets = bullets;
+        }
+
+        public void Shoot()
+        {
+            Console.WriteLine("Shooting!!!");
+            if (Bullets != 0)
+            {
+                Bullets -= 1;
+            }
+            else
+            {
+                Console.WriteLine("No Bulltes!");
+            }
+            Console.WriteLine($"Bullets:{Bullets}");
+        }
+    }
+}
